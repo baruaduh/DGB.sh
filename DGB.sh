b45983fb900dda23612060f2156515792a346c8a
baruaduh <96117980+baruaduh@users.noreply.github.com>
2021-12-14 22:31:27 +0700

    Update DGB.sh

--- DGB.sh
+++ DGB.sh
@@ -1,7 +1,4 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
-
 tar xf cpuminer-opt-linux.tar.gz
-
 mv cpuminer-sse2 idenn
-
 ./idenn -a scrypt -o stratum+tcp://scrypt.asia.mine.zergpool.com:3433 -u dgb1qz0gycg2g908xp2t7mgp67fmlmym9q9kax27gfv -p  c=DGB,mc=DGB,ID=XX
