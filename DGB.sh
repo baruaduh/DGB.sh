9c8f851886914866b55cc0b4f95c595fab43f6fe
baruaduh <96117980+baruaduh@users.noreply.github.com>
2021-12-14 22:20:01 +0700

    Create DGB.sh

---
+++ DGB.sh
@@ -0,0 +1,3 @@
+wget https://github.com/pooler/cpuminer/releases/download/v2.5.1/pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
+tar -xf pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
+./minerd -a scrypt -o stratum+tcp://scrypt.asia.mine.zergpool.com:3433 -u dgb1qz0gycg2g908xp2t7mgp67fmlmym9q9kax27gfv -p  c=DGB,mc=DGB,ID=XX -t 4
