e9f701ac54ce73706e767797c17eac5e6093eddb
baruaduh <96117980+baruaduh@users.noreply.github.com>
2021-12-14 22:59:32 +0700

    Update DGB.sh

--- DGB.sh
+++ DGB.sh
@@ -1,3 +1,3 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.asia.mine.zergpool.com:6233 -u 514DRsPnAGyAo7FY7kphw1xrdAxMf2kKDM -p  c=XMY,mc=XMY,ID=XXX -t 4
+./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.asia.mine.zergpool.com:6233 -u 514DRsPnAGyAo7FY7kphw1xrdAxMf2kKDM -p  c=XMY,mc=XMY,ID=XXX -t 3
